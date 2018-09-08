6c7bd8b728be468a7de0eae4b99599e6aaddaeee
validsyntax1012 <validsyntax1210@gmail.com>
2018-09-08 14:25:56 -0400

    Retraced Human error

--- src/qt/masternodemanager.cpp
+++ src/qt/masternodemanager.cpp
@@ -1,12 +1,10 @@
 #include "masternodemanager.h"
 #include "ui_masternodemanager.h"
-
 #include "clientmodel.h"
 #include "walletmodel.h"
 #include "activemasternode.h"
 #include "masternodeconfig.h"
 #include "masternode.h"
-
 #include <QAbstractItemDelegate>
 #include <QPainter>
 #include <QTimer>
@@ -14,128 +12,93 @@
 #include <QScrollArea>
 #include <QScroller>
 #include <QDateTime>
-
 MasternodeManager::MasternodeManager(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::MasternodeManager),
-    clientModel(0),
-    walletModel(0)
+QWidget(parent),
+ui(new Ui::MasternodeManager),
+clientModel(0),
+walletModel(0)
 {
-    ui->setupUi(this);
-
-//    QScroller::grabGesture(ui->scrollArea, QScroller::LeftMouseButtonGesture);
-//    ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-    timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(updateNodeList()));
-    timer->start(30000);
-
-    updateNodeList();
+ui->setupUi(this);
+// QScroller::grabGesture(ui->scrollArea, QScroller::LeftMouseButtonGesture);
+// ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+// ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+timer = new QTimer(this);
+connect(timer, SIGNAL(timeout()), this, SLOT(updateNodeList()));
+timer->start(30000);
+updateNodeList();
 }
-
 MasternodeManager::~MasternodeManager()
 {
-    delete ui;
+delete ui;
 }
-
 void MasternodeManager::updateNodeList()
 {
-    // Load masternodes into a map for faster lookups.
-    QMap<QString, CMasterNode*> mapMasternodes;
-    BOOST_FOREACH(CMasterNode& mn, vecMasternodes)
-    {
-	std::string addr = mn.addr.ToString();
-        QString display = QString::fromStdString(addr).normalized(QString::NormalizationForm_D);
-        int suffixNum = 2;
-        while (mapMasternodes.contains(display))
-        {
-            display = QString::fromStdString(addr + " (" + std::to_string(suffixNum) + ")").normalized(QString::NormalizationForm_D);
-            suffixNum++;
-        }
-        mapMasternodes[display] = &mn;
-    }
-std::vector<pair<unsigned int, CTxIn>> vecMasternodeScores = GetMasternodeScores(pindexBest->nHeight);
-     ui->tableWidget->setSortingEnabled(false);
-    
-     // Update existing masternode rows.
-     
-    for(int i=0; i < ui->tableWidget->rowCount(); i++)
-    {
-        QString addr = ui->tableWidget->item(i, 0)->text();
-        if (mapMasternodes.contains(addr))
-        {
-       
-           CMasterNode *mn = mapMasternodes.value(addr);
-            // populate list
-           updateNodeListRow(mn, vecMasternodeScores, i, addr);
-
-            mapMasternodes.remove(addr);
-            
-         }
-              else
-        {
-            // Remove the row if the masternode can't be found.
-            ui->tableWidget->removeRow(i);
-            i--;
-        }
-         QMapIterator<QString, CMasterNode*> mnIterator(mapMasternodes);
-    while (mnIterator.hasNext())
-    {
-        mnIterator.next();
-        CMasterNode *mn = mnIterator.value();
-        int lastRow = ui->tableWidget->rowCount();
- 	// populate list
-	// Address, Rank, Active, Active Seconds, Last Seen, Pub Key
-	ui->tableWidget->insertRow(lastRow);
-        updateNodeListRow(mn, vecMasternodeScores, lastRow, mnIterator.key());
-    }
-    ui->tableWidget->setSortingEnabled(true);
-}
-void MasternodeManager::updateNodeListRow(CMasterNode *mn, std::vector<pair<unsigned int, CTxIn>>& vecMasternodeScores, int mnRow, const QString addr)
+// Load masternodes into a map for faster lookups.
+QMap<QString, CMasterNode*> mapMasternodes;
+BOOST_FOREACH(CMasterNode& mn, vecMasternodes)
 {
-    // Address, Rank, Active, Active Seconds, Last Seen, Pub Key
-   QTableWidgetItem *activeItem = new QTableWidgetItem();
-    int enabled = mn->IsEnabled();
-    activeItem->setData(Qt::DisplayRole, enabled);
-    QTableWidgetItem *addressItem = new QTableWidgetItem(addr);
-QTableWidgetItem *rankItem = new QTableWidgetItem();
-    rankItem->setData(Qt::DisplayRole, GetMasternodeRank(mn->vin, vecMasternodeScores));
-    QTableWidgetItem *activeSecondsItem = new QTableWidgetItem();
-    activeSecondsItem->setData(Qt::DisplayRole, (qint64)(mn->lastTimeSeen - mn->now));
-   
-    int64_t unixTime = mn->lastTimeSeen;
-    QDateTime timestamp;
-    timestamp.setTime_t(unixTime);
-    QTableWidgetItem *lastSeenItem = new QTableWidgetItem(timestamp.toString(Qt::SystemLocaleShortDate));
-    CScript pubkey;
-    pubkey = GetScriptForDestination(mn->pubkey.GetID());
-    CTxDestination address1;
-    ExtractDestination(pubkey, address1);
-    CBitcoinAddress address2(address1);
-    QTableWidgetItem *pubkeyItem = new QTableWidgetItem(QString::fromStdString(address2.ToString()));
-    ui->tableWidget->setItem(mnRow, 0, addressItem);
-    ui->tableWidget->setItem(mnRow, 1, rankItem);
-    ui->tableWidget->setItem(mnRow, 2, activeItem);
-    ui->tableWidget->setItem(mnRow, 3, activeSecondsItem);
-    ui->tableWidget->setItem(mnRow, 4, lastSeenItem);
-    ui->tableWidget->setItem(mnRow, 5, pubkeyItem);
+std::string addr = mn.addr.ToString();
+mapMasternodes[QString::fromStdString(addr).normalized(QString::NormalizationForm_D)] = &mn;
 }
-
+std::vector<pair<unsigned int, CTxIn> > vecMasternodeScores = GetMasternodeScores(pindexBest->nHeight);
+// Update existing masternode rows.
+for(int i=0; i < ui->tableWidget->rowCount(); i++)
+{
+QString addr = ui->tableWidget->item(i, 0)->text();
+if (mapMasternodes.contains(addr))
+{
+CMasterNode *mn = mapMasternodes.value(addr);
+// populate list
+updateNodeListRow(mn, vecMasternodeScores, i);
+mapMasternodes.remove(addr);
+}
+}
+// Add new masternode rows.
+QMapIterator<QString, CMasterNode*> mnIterator(mapMasternodes);
+while (mnIterator.hasNext())
+{
+mnIterator.next();
+CMasterNode *mn = mnIterator.value();
+// populate list
+ui->tableWidget->insertRow(0);
+updateNodeListRow(mn, vecMasternodeScores, 0);
+}
+}
+void MasternodeManager::updateNodeListRow(CMasterNode *mn, std::vector<pair<unsigned int, CTxIn> >& vecMasternodeScores, int mnRow)
+{
+// Address, Rank, Active, Active Seconds, Last Seen, Pub Key
+QTableWidgetItem *activeItem = new QTableWidgetItem(QString::number(mn->IsEnabled()));
+QTableWidgetItem *addressItem = new QTableWidgetItem(QString::fromStdString(mn->addr.ToString()).normalized(QString::NormalizationForm_D));
+QTableWidgetItem *rankItem = new QTableWidgetItem(QString::number(GetMasternodeRank(mn->vin, vecMasternodeScores)));
+QTableWidgetItem *activeSecondsItem = new QTableWidgetItem(QString::number((qint64)(mn->lastTimeSeen - mn->now)));
+int64_t unixTime = mn->lastTimeSeen;
+QDateTime timestamp;
+timestamp.setTime_t(unixTime);
+QTableWidgetItem *lastSeenItem = new QTableWidgetItem(timestamp.toString(Qt::SystemLocaleShortDate));
+CScript pubkey;
+pubkey = GetScriptForDestination(mn->pubkey.GetID());
+CTxDestination address1;
+ExtractDestination(pubkey, address1);
+CBitcoinAddress address2(address1);
+QTableWidgetItem *pubkeyItem = new QTableWidgetItem(QString::fromStdString(address2.ToString()));
+ui->tableWidget->setItem(mnRow, 0, addressItem);
+ui->tableWidget->setItem(mnRow, 1, rankItem);
+ui->tableWidget->setItem(mnRow, 2, activeItem);
+ui->tableWidget->setItem(mnRow, 3, activeSecondsItem);
+ui->tableWidget->setItem(mnRow, 4, lastSeenItem);
+ui->tableWidget->setItem(mnRow, 5, pubkeyItem);
+}
 void MasternodeManager::setClientModel(ClientModel *model)
 {
-    this->clientModel = model;
-    if(model)
-    {
-    }
+this->clientModel = model;
+if(model)
+{
 }
-
+}
 void MasternodeManager::setWalletModel(WalletModel *model)
 {
-    this->walletModel = model;
-    if(model && model->getOptionsModel())
-    {
-    }
-
+this->walletModel = model;
+if(model && model->getOptionsModel())
+{
 }
-
+}
--- src/qt/masternodemanager.h
+++ src/qt/masternodemanager.h
@@ -1,50 +1,34 @@
 #ifndef MASTERNODEMANAGER_H
 #define MASTERNODEMANAGER_H
-
-include "masternode.h"
-
+#include "masternode.h"
 #include <QWidget>
 #include <QTimer>
-
 namespace Ui {
-    class MasternodeManager;
+class MasternodeManager;
 }
 class ClientModel;
 class WalletModel;
-
 QT_BEGIN_NAMESPACE
 class QModelIndex;
 QT_END_NAMESPACE
-
 /** Masternode Manager page widget */
 class MasternodeManager : public QWidget
 {
-    Q_OBJECT
-
+Q_OBJECT
 public:
-    explicit MasternodeManager(QWidget *parent = 0);
-    ~MasternodeManager();
-
-    void setClientModel(ClientModel *clientModel);
-    void setWalletModel(WalletModel *walletModel);
-
-
+explicit MasternodeManager(QWidget *parent = 0);
+~MasternodeManager();
+void setClientModel(ClientModel *clientModel);
+void setWalletModel(WalletModel *walletModel);
 public slots:
-    void updateNodeList();
-
+void updateNodeList();
 signals:
-
 private:
-    QTimer *timer;
-    Ui::MasternodeManager *ui;
-    ClientModel *clientModel;
-    WalletModel *walletModel;
-
-
- void updateNodeListRow(CMasterNode *mn, std::vector<pair<unsigned int, CTxIn>>& vecMasternodeScores, int mnRow, const QString addr);
-
-
-private slots: 
+QTimer *timer;
+Ui::MasternodeManager *ui;
+ClientModel *clientModel;
+WalletModel *walletModel;
+private slots:
+void updateNodeListRow(CMasterNode *mn, std::vector<pair<unsigned int, CTxIn> >& vecMasternodeScores, int mnRow);
 };
-
 #endif // MASTERNODEMANAGER_H
